787458f9e63267dd767fcfcb4b8c39db16db59e7
Birgit Pauli-Haack <birgit.pauli@gmail.com>
2026-08-13 15:04:36 +0200

    Update to new header and overlay

--- patterns/home.php
+++ patterns/home.php
@@ -5,7 +5,7 @@
  * Inserter: no
  */
 ?>
-<!-- wp:template-part {"slug":"vertical-header"} /-->
+<!-- wp:template-part {"slug":"header"} /-->
 
 <!-- wp:group {"tagName":"main","style":{"spacing":{"blockGap":"0"}},"layout":{"type":"constrained"}} -->
 <main class="wp-block-group"><!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"layout":{"type":"constrained"}} -->
